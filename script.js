21fba697b1478fff2b9769c6f5c826b9548b1688
I don't know <lea.granier@outlook.fr>
2024-01-05 18:54:24 +0100

    add function for add and remove hover + japan

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
     const canada = document.querySelectorAll('.Canada');
+    const japan = document.querySelectorAll('.Japan')
 
-    canada.forEach(function (element) {
-        element.addEventListener('mouseover', function () {
-            canada.forEach(function (el) {
-                el.classList.add('hovered');
+    function addHoverEffect(elements, className) {
+        elements.forEach(function (element) {
+            element.addEventListener('mouseover', function () {
+                elements.forEach(function (el) {
+                    el.classList.add(className);
+                });
+            });
+
+            element.addEventListener('mouseout', function () {
+                elements.forEach(function (el) {
+                    el.classList.remove(className);
+                });
             });
         });
+    }
 
-        element.addEventListener('mouseout', function () {
-            canada.forEach(function (el) {
-                el.classList.remove('hovered');
-            });
-        });
-    });
-});
+    addHoverEffect(canada, 'hovered');
+    addHoverEffect(japan, 'hovered');
+})
