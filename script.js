71dbe7924a8aadae55281459709ed3b66de4b155
I don't know <lea.granier@outlook.fr>
2024-01-05 19:04:24 +0100

    add a clicked event in function

--- script.js
+++ script.js
@@ -5,20 +5,40 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function addHoverEffect(elements, className) {
         elements.forEach(function (element) {
+            var clicked = false;
+
             element.addEventListener('mouseover', function () {
-                elements.forEach(function (el) {
-                    el.classList.add(className);
-                });
+                if (!clicked) {
+                    elements.forEach(function (el) {
+                        el.classList.add(className);
+                    });
+                }
             });
 
             element.addEventListener('mouseout', function () {
-                elements.forEach(function (el) {
-                    el.classList.remove(className);
-                });
+                if (!clicked) {
+                    elements.forEach(function (el) {
+                        el.classList.remove(className);
+                    });
+                }
+            });
+
+            element.addEventListener('click', function () {
+                clicked = !clicked;
+
+                if (clicked) {
+                    elements.forEach(function (el) {
+                        el.classList.add(className);
+                    });
+                } else {
+                    elements.forEach(function (el) {
+                        el.classList.remove(className);
+                    });
+                }
             });
         });
     }
-
+    
     addHoverEffect(canada, 'hovered');
     addHoverEffect(japan, 'hovered');
     addHoverEffect(norway, 'hovered');
