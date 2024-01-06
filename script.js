80e04e9f09253093c600eeccb1b76d7e8d971861
I don't know <lea.granier@outlook.fr>
2024-01-06 22:21:02 +0100

    add Australia hover

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const canada = document.querySelectorAll('.Canada');
     const japan = document.querySelectorAll('.Japan')
     const norway = document.querySelectorAll('.Norway')
+    const australia = document.querySelectorAll('.Australia')
 
     function addHoverEffect(elements, className) {
         elements.forEach(function (element) {
@@ -42,4 +43,5 @@ document.addEventListener('DOMContentLoaded', function () {
     addHoverEffect(canada, 'hovered');
     addHoverEffect(japan, 'hovered');
     addHoverEffect(norway, 'hovered');
+    addHoverEffect(australia, 'hovered')
 })
